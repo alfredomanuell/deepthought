0549641db74450ef441d4808b79b2a495ac93507
Welepy-dev <mwsilva.contato@gmail.com>
2026-05-18 20:58:55 +0100

    refactor: update main.tsx to use AppRouter instead of App component

--- frontend/deepthought/src/main.tsx
+++ frontend/deepthought/src/main.tsx
@@ -1,11 +1,12 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+import { AppRouter } from './routes'
+
 
 createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
-		<App/>
+		<AppRouter></AppRouter>
 	</BrowserRouter>,
 )
