96d09f09e01ab96f0d016c9934d16f979bbf719a
Welepy-dev <mwsilva.contato@gmail.com>
2026-05-19 19:35:56 +0100

    feat: enhance App component with CRT filter overlay and update styles; add new routes for Landing and TestPage

--- frontend/deepthought/src/routes/index.tsx
+++ frontend/deepthought/src/routes/index.tsx
@@ -3,14 +3,17 @@ import App from "../App";
 import EmailSignIn from "../signin/email_signin";
 import SignIn from "../signin/signin";
 import OTPEmail from "../signin/OTPemail";
+import Landing from "../pages/Landing";
+import TestPage from "../pages/TestPage";
 
 export function AppRouter() {
 	return (
 			<Routes>
 				<Route path="/" element=<App CustomComponent={SignIn}/>/>
 				<Route path="/EmailSignIn" element=<App CustomComponent={EmailSignIn}/>/>
-				<Route path="/OTPEmail" element=<App CustomComponent={OTPEmail}/>/>
-
+				<Route path="/OTPEmail" element=<App CustomComponent={OTPEmail}/>/> 
+				<Route path="/Landing" element=<App CustomComponent={Landing}/>/> 
+				<Route path="/test" element=<App CustomComponent={TestPage}/>/> 
 			</Routes>
 
 	)
