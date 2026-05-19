31639dd352dabd51e5f78c06f98ea9b6830b1904
Welepy-dev <mwsilva.contato@gmail.com>
2026-05-19 19:37:29 +0100

    adding route option to custom button, maybe in the future it should have a ternary

--- frontend/deepthought/src/components/CustomButton.tsx
+++ frontend/deepthought/src/components/CustomButton.tsx
@@ -1,6 +1,9 @@
-export default function CustomButton({name} : {name : string}){
+import { useNavigate } from "react-router-dom"
+
+export default function CustomButton({name, route} : {name : string, route : string}){
+	const navigate = useNavigate();
 	return (
-		<button className="text-neutral_contrast block mt-14 mx-auto font-pressStart text-[14px] 
+		<button onClick={() => navigate(route)} className="text-neutral_contrast block mt-4 mx-auto font-pressStart text-[14px] 
 			bg-contrast border-black border-t-4 border-l-4 border-b-8 border-r-8 hover:border-t-4 hover:border-l-4 hover:border-b-4 hover:border-r-4
 			hover:translate-x-1 hover:translate-y-1
 			transition-all duration-100 hover:bg-secundary
