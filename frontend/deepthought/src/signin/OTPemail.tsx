a02b21bdaf1ebc112a530fedbb5a9990d3f51eec
Welepy-dev <mwsilva.contato@gmail.com>
2026-05-19 19:36:18 +0100

    OTP email finished for now

--- frontend/deepthought/src/signin/OTPemail.tsx
+++ frontend/deepthought/src/signin/OTPemail.tsx
@@ -2,12 +2,12 @@ import emailIcon from '../assets/open_email.png'
 
 export default function OTPEmail() {
 	return (
-			<div className="flex flex-col items-center w-1/2 xl:w-2/5 h-96 bg-neutral_contrast border-b-8 border-r-8 border-l-4 border-t-4 border-black">
+			<div className="flex flex-col items-center w-[500px]  h-96 bg-neutral_contrast border-b-8 border-r-8 border-l-4 border-t-4 border-black">
 				<div className='flex flex-col items-center'>
 					<img src={ emailIcon } alt="Email icon" className='w-24 h-auto'/>
 				</div>
-				<input className="border-b-8 border-r-8 border-l-4 border-t-4 border-black" type="poggers" />
-				<p className='font-pressStart text-contrast text-xs'>Write the code sent to you.</p>
+				<input placeholder="* * * * * *" className="text-center py-2 text-sm font-pressStart focus:outline-none mt-4 border-b-8 border-r-8 border-l-4 border-t-4 border-black" type="poggers" />
+				<p className='pt-4 font-pressStart text-contrast text-xs'>Write the code sent to you.</p>
 			</div>
 	)
 }
