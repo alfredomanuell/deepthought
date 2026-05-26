d6488429a6c9499cb16a88a4f0c3b332475b7c24
11StarLord <diogowalter114@gmail.com>
2026-05-26 01:16:33 +0100

    Adding feedback to the backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,10 @@ import { AchivementsModule } from './achivements/achivements.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ResourcesModule } from './resources/resources.module';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
+import { FeedbackModule } from './feedback/feedback.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, AchivementsModule, ProjectsModule, ResourcesModule, LeaderboardModule],
+  imports: [AuthModule, UsersModule, AchivementsModule, ProjectsModule, ResourcesModule, LeaderboardModule, FeedbackModule],
   controllers: [AppController],
   providers: [AppService],
 })
