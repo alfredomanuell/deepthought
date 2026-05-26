e390ce88fdcc3c39e0029ff8b79cf55791c841be
11StarLord <diogowalter114@gmail.com>
2026-05-26 01:09:41 +0100

    Adding resources to the backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AchivementsModule } from './achivements/achivements.module';
 import { ProjectsModule } from './projects/projects.module';
+import { ResourcesModule } from './resources/resources.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, AchivementsModule, ProjectsModule],
+  imports: [AuthModule, UsersModule, AchivementsModule, ProjectsModule, ResourcesModule],
   controllers: [AppController],
   providers: [AppService],
 })
